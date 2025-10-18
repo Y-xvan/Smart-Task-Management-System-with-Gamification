82cdd4876a60a282bb768ca7895a85f964c46881
Y-xvan <124090828@link.cuhk.edu.cn>
2025-10-18 13:11:40 +0800

    Add DAOFactory header file for data access objects

---
+++ include/database/DAOFactory.h
@@ -0,0 +1,38 @@
+#ifndef DAO_FACTORY_H
+#define DAO_FACTORY_H
+
+#include "TaskDAO.h"
+#include "ProjectDAO.h"
+#include "ReminderDAO.h"
+#include "ChallengeDAO.h"
+#include "AchievementDAO.h"
+#include "SettingsDAO.h"
+#include "ExperienceDAO.h"
+#include "StatisticsDAO.h"
+#include "MigrationManager.h"
+#include <memory>
+
+class DAOFactory {
+public:
+    virtual ~DAOFactory() = default;
+    
+    // DAO创建方法
+    virtual std::unique_ptr<TaskDAO> createTaskDAO() = 0;
+    virtual std::unique_ptr<ProjectDAO> createProjectDAO() = 0;
+    virtual std::unique_ptr<ReminderDAO> createReminderDAO() = 0;
+    virtual std::unique_ptr<ChallengeDAO> createChallengeDAO() = 0;
+    virtual std::unique_ptr<AchievementDAO> createAchievementDAO() = 0;
+    virtual std::unique_ptr<SettingsDAO> createSettingsDAO() = 0;
+    virtual std::unique_ptr<ExperienceDAO> createExperienceDAO() = 0;
+    virtual std::unique_ptr<StatisticsDAO> createStatisticsDAO() = 0;
+    virtual std::unique_ptr<MigrationManager> createMigrationManager() = 0;
+    
+    // 数据库状态
+    virtual bool isDatabaseReady() = 0;
+    virtual bool initializeDatabase() = 0;
+    
+    // 工厂实例
+    static std::unique_ptr<DAOFactory> createFactory();
+};
+
+#endif // DAO_FACTORY_H
