8d0cf95215da2a4f492e6d8a015574b0e42c826b
leetcode0512 <fyf18868811717@gmail.com>
2025-11-20 11:52:32 +0800

    Refactor AchievementDAO to use 'achievementKey'
    Updated the AchievementDAO class to use 'achievementKey' instead of 'achievementName' for consistency.

--- include/database/DAO/AchievementDAO.h
+++ include/database/DAO/AchievementDAO.h
@@ -2,15 +2,15 @@
 #include <string>
 #include <vector>
 #include <map>
-#include "common/entities.h"
+#include "entities.h"
 
 class AchievementDAO {
 private:
     std::string dataFilePath;
-    
+
     std::vector<Achievement> achievementDefinitions;
     std::vector<Achievement> userAchievements;
-    
+
     std::string getDefinitionFilePath() const;
     std::string getUserAchievementFilePath(int userId) const;
     bool fileExists(const std::string& filename) const;
@@ -19,31 +19,33 @@ private:
 public:
     AchievementDAO();
     explicit AchievementDAO(const std::string& basePath);
-    
+
     bool loadAchievementDefinitions();
     bool saveAchievementDefinitions();
     std::vector<Achievement> getAllAchievementDefinitions() const;
-    Achievement getAchievementDefinition(const std::string& achievementName) const;
-    
+    Achievement getAchievementDefinition(const std::string& achievementKey) const;
+
     bool loadUserAchievements(int userId);
     bool saveUserAchievements(int userId);
     std::vector<Achievement> getUserAchievements(int userId) const;
-    Achievement* getUserAchievement(int userId, const std::string& achievementName);
-    
-    bool unlockAchievement(int userId, const std::string& achievementName);
-    bool updateAchievementProgress(int userId, const std::string& achievementName, int progress);
+    Achievement* getUserAchievement(int userId, const std::string& achievementKey);
+
+    bool unlockAchievement(int userId, const std::string& achievementKey);
+    bool updateAchievementProgress(int userId, const std::string& achievementKey, int progress);
     bool resetUserAchievements(int userId);
-    
+
     int getUnlockedAchievementCount(int userId) const;
     int getTotalXP(int userId) const;
     std::vector<Achievement> getRecentlyUnlockedAchievements(int userId, int count = 5) const;
-    
+
     void initializeDefaultAchievements();
     bool userAchievementFileExists(int userId) const;
-    
+
     static std::string getCurrentTimestamp();
-    
+
 private:
     int generateAchievementId();
     int nextAchievementId = 1;
 };
+
+
